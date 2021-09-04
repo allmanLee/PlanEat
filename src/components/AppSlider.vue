bf9bec82d4c9770a36ef0b5d4aa1fbb91d036526
leeyoujun <muenzz119@naver.com>
2021-09-04 20:47:13 +0900

    feat: app appSwiper commponent

---
+++ src/components/AppSlider.vue
@@ -0,0 +1,39 @@
+<template>
+  <swiper>
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+  </swiper>
+</template>
+<script lang="ts">
+/* tslint:disable:no-unused-variable */
+import SwiperCore from "swiper";
+import { defineComponent } from "vue";
+import { IonicSwiper } from "@ionic/vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+SwiperCore.use([IonicSwiper]);
+export default defineComponent({
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+
+  setup() {
+    const onSwiper = (swiper: any) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log("slide change");
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+    };
+  },
+});
+</script>
+<style scoped>
+</style>
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,10 +1,11 @@
 <template>
   <ion-page>
     <app-header :headerTitle="'레시피'"></app-header>
+    <ion-header>
+      <app-segment :emitSegmentValue="fetchSegmentValue"></app-segment>
+    </ion-header>
     <ion-content class="b">
-      <ion-header>
-        <app-segment :emitSegmentValue="fetchSegmentValue"></app-segment>
-      </ion-header>
+      <app-slider></app-slider>
     </ion-content>
   </ion-page>
 </template>
@@ -14,12 +15,14 @@ import { defineComponent } from "vue";
 import AppHeader from "@/components/AppHeader.vue";
 import AppSegment from "@/components/AppSegment.vue";
 import { IonPage } from "@ionic/vue";
+import AppSlider from "@/components/AppSlider.vue";
 
 export default defineComponent({
   components: {
     AppHeader,
     AppSegment,
     IonPage,
+    AppSlider,
   },
   setup() {
     let segmentIndex = 0;
