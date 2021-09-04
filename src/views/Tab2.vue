a718e63d23c28b263d7f6957515f1fc88c08d5fb
leeyoujun <muenzz119@naver.com>
2021-09-04 14:59:48 +0900

    feat: add tab2 segment component function
    세그먼트 태그의 값이 바뀔때 emit 을 통하여 값을 부모컴포넌트로 넘긴다.

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,17 +1,36 @@
 <template>
   <ion-page>
     <app-header :headerTitle="'레시피'"></app-header>
+    <ion-content class="b">
+      <ion-header>
+        <app-segment :emitSegmentValue="fetchSegmentValue"></app-segment>
+      </ion-header>
+    </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
+import { defineComponent } from "vue";
 import AppHeader from "@/components/AppHeader.vue";
+import AppSegment from "@/components/AppSegment.vue";
 import { IonPage } from "@ionic/vue";
 
-export default {
+export default defineComponent({
   components: {
     AppHeader,
+    AppSegment,
     IonPage,
   },
-};
+  setup() {
+    let segmentIndex = 0;
+    const fetchSegmentValue = (val: any) => {
+      segmentIndex = val;
+    };
+    return { segmentIndex, fetchSegmentValue };
+  },
+});
 </script>
+
+
+<style scoped>
+</style>
